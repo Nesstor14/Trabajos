2526c5c82662a0e7266b6bc872cee4636d8980a0
Nesstor14 <natipan14@gmail.com>
2024-09-03 22:48:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-
+<form action="calcular.php" method="post">
+  <h2>Ingrese los datos de los productos</h2>
+  <?php for ($i = 1; $i <= 10; $i++) { ?>
+    <label>Producto <?php echo $i; ?>:</label>
+    <input type="number" name="cantidad[]" placeholder="Cantidad">
+    <input type="number" name="precio[]" placeholder="Precio">
+    <br>
+  <?php } ?>
+  <input type="submit" value="Calcular">
+</form>
